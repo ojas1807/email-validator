9ea9193e7acbe6abb2c2f08a11b5f2bb449331e3
ojas1807 <ojaspatrikar15@gmail.com>
2025-07-15 16:31:05 +0530

    Fix: Bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -29,4 +29,6 @@ def download_file(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
